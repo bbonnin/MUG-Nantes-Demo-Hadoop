cc0469f5fa1f96d3f746358eff8a6edeec19ed79
Bruno Bonnin <bbonnin@gmail.com>
2015-09-29 09:48:27 -0700

    Update spark python

--- spark/python/connector-demo.py
+++ spark/python/connector-demo.py
@@ -1,76 +1,51 @@
 import math
 from pyspark import SparkContext
 
+
 #sc = SparkContext("spark://quickstart.cloudera:7077", "Test MongoDB Connector")
 sc = SparkContext("local", "Test MongoDB Connector")
 
 
 # Config MongoDB
-config = { "mongo.input.uri" : "mongodb://localhost:27017/marketdata.minbars", "mongo.output.uri" : "mongodb://localhost:27017/marketdata.maxminbars" }
+inputConfig = { "mongo.input.uri" : "mongodb://localhost:27017/marketdata.stock_prices" }
+outputConfig = { "mongo.output.uri" : "mongodb://localhost:27017/marketdata.maxminprices" }
 
 # Config pour RDD qui va lire les data dans MongoDB
 inputFormatClassName = "com.mongodb.hadoop.MongoInputFormat"
-keyClassName = "org.apache.hadoop.io.Text"
-valueClassName = "org.apache.hadoop.io.MapWritable"
+keyClassName = "java.lang.Object"
+valueClassName = "org.bson.BSONObject"
 
-minBarRawRDD = sc.newAPIHadoopRDD(inputFormatClassName, keyClassName, valueClassName, None, None, config)
+stockPricesRDD = sc.newAPIHadoopRDD(inputFormatClassName, keyClassName, valueClassName, None, None, inputConfig)
 
-#config["mongo.output.uri"] = "mongodb://localhost:27017/marketdata.maxminbars"
 # Config pour RDD qui va ecrire dans MongoDB
-outputFromatClassName = "com.mongodb.hadoop.MongoOutputFormat"
+outputFormatClassName = "com.mongodb.hadoop.MongoOutputFormat"
 
 # Les traitements...
-
-def mm(doc1, doc2):
-    print "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO"
-    print doc1
-    print doc2
-    print "oooooooooooooooooooooooooooooooooooooooooooooooooo"
-    return doc1
-
 # ... sur l'ensemble des data
-#minBarRDD = minBarRawRDD.values()
-minBarRDD = sc.parallelize([{"Symbol":"M","Day":1,"High":1}, {"Symbol":"M","Day":1,"High":2}, {"Symbol":"M","Day":2,"High":3}]) 
-
-result = minBarRDD.groupBy(lambda doc: (doc["Symbol"], doc["Day"])) \
-                  .reduceByKey(mm) \
-                  .collect()
-
-#dataSource = sc.parallelize( [("user1", "film"), ("user1", "film"), ("user2", "film"), ("user2", "books"), ("user2", "books")] )
-#dataReduced = dataSource.reduceByKey(lambda x,y : x + "," + y)
-#result = dataReduced.map(lambda (user,values) : (values.split(","),user))
-
+prices = stockPricesRDD.values()
 
 # ... groupby sur (symbol, day)
-#groupByRDD = minBarRDD.groupBy(lambda doc: (doc["Symbol"], doc["Day"]))
+groupByRDD = prices.groupBy(lambda doc: (doc["Symbol"], doc["Day"]))
+#                  .map(lambda tuple: (tuple[0], tuple[1])) \
+#                  .collect()
 
-# ... reduce par clef (on prend le max de High et le min de Low)
-#reduceRDD = groupByRDD.reduceByKey(lambda doc1, doc2: { "High1" : math.max(doc1["High"], doc2["High"]), "Low1" : math.min(doc1["Low"], doc2["Low"])})
+# ... aggregate par clef (on prend le max de High et le min de Low)
+def maxMin(doc, groupedDocs):
+    for gdoc in groupedDocs:
+        doc = { "High" : max(doc.get("High", 0), gdoc["High"]), "Low" : min(doc.get("Low", 99999), gdoc["Low"]) }
+    return doc
 
-#reduceRDD = groupByRDD.reduceByKey(mm)
+aggRDD = groupByRDD.aggregateByKey(
+    { },
+    maxMin,
+    lambda doc1, doc2: { "High" : max(doc1["High"], doc2["High"]), "Low" : min(doc1["Low"], doc2["Low"])})
 
-# ... trie sur les clefs
-#sortRDD = reduceRDD.sortByKey()
-
+# ... sauve dans HDFS
+aggRDD.saveAsTextFile("hdfs:///user/cloudera/data/spark_result")
+ 
 # ... collecte du tout
-#result = sortRDD.collect()
+#result = aggRDD.collect()
 
-#print "**************** count = %d" % len(result)
-
-print result
-for doc in result:
-    print doc[0], doc[1]
-    for v in doc[1]:
-        print "   " , v
-        print "************************************"
-
-def maxMin(groupedSymbols):
-    print "*******************************************************"
-    for doc in groupedSymbols:
-        print "doc %s" % doc
-    outputDoc = { "Symbol" : groupedSymbols[0][0] }
-    print "======================================================="
-    return (None, outputDoc)
-
-#resultRDD = symbols.map(maxMin)
+#for doc in result:
+#    print doc[0], doc[1]
 
--- spark/run-py-connector-demo.sh
+++ spark/run-py-connector-demo.sh
@@ -3,5 +3,5 @@
 script_path=$(dirname "${BASH_SOURCE[0]}")
 mongo_libs=${script_path}/../mongodb/libs
 
-spark-submit --jars ${mongo_libs}/mongo-hadoop-core-1.4.0.jar,${mongo_libs}/mongo-java-driver-3.0.3.jar ${script_path}/connector-demo.py
+spark-submit --jars ${mongo_libs}/mongo-hadoop-core-1.4.0.jar,${mongo_libs}/mongo-java-driver-3.0.3.jar ${script_path}/python/connector-demo.py
 
